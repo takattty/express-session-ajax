ba0981582ebe0b76d8aec0be72053f94df028025
takattty <r9r_celtics_t9t@icloud.com>
2019-07-12 00:38:22 +0900

    change hello.js file

--- routes/hello.js
+++ routes/hello.js
@@ -2,20 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    const name = req.query.name;
-    const mail = req.query.mail;
-    const data = {
+    let msg = '何か書いてください!!';
+    if (req.session.message != undefined) {
+        msg = "Last Message: " + req.session.message;
+    }
+    let data = {
         title: 'Hello!',
-        content: '何か書いて送信して下さい。'
+        content: msg
     };
     res.render('hello', data);
 });
 
 router.post('/post', (req, res, next) => {
-    const msg = req.body['message'];
-    const data = {
-        title: 'Hello!',
-        content: 'あなたは、「' + msg + '」と送りました。'
+    let msg = req.body['message'];
+    req.session.message = msg;
+    let data = {
+        title: 'Hello　post!',
+        content: 'Last Message: ' + req.session.message
     };
     res.render('hello', data);
 });
