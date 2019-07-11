e1217512d74a11a0a40f3df42a6c881e72685072
takattty <r9r_celtics_t9t@icloud.com>
2019-07-11 22:57:50 +0900

    create hello files

--- routes/hello.js
+++ routes/hello.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    const name = req.query.name;
-    const mail = req.query.mail;
-    const date = {
+    let name = req.query.name;
+    let mail = req.query.mail;
+    let data = {
         title: 'Hello!',
         content: 'あなたの名前は、' + name + '。<br>' +
                 'メールアドレスは、' + mail + 'です。'
     };
-    res.render('hello', date);
+    res.render('hello', data);
 });
 
 module.exports = router;
