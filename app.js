9fd2b1f9b0b941b297bba324797fd337d5f27ee7
takattty <r9r_celtics_t9t@icloud.com>
2019-07-11 23:49:13 +0900

    change app.js file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,13 +18,21 @@ console.log('Server start!!!!!!!!');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const session_opt = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 1000 } 
+};
+
+app.use(session(session_opt));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));//この２行がURLエンコードとJSONエンコーディングをONにしている。
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 
